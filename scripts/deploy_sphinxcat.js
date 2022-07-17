8e878fef9675ea9769c7fa2a18dc6056ec1a9aed
Evans Yang <evans@lemoon.work>
2022-07-17 21:35:14 +0800

    add deploy logs

--- scripts/deploy_sphinxcat.js
+++ scripts/deploy_sphinxcat.js
@@ -7,6 +7,8 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 const utils = hre.ethers.utils;
 const { merkleTree } = require("./old/merkletree_util")
+const { connect, log_gas_price } = require('./common.js')
+
 require("dotenv").config();
 
 // javascript 日期中，Month是从0开始编号的
@@ -22,13 +24,16 @@ const BigNumber = ethers.BigNumber
 // deploy resources
 async function deploy() {
   const root = merkleTree.getRoot()
-  
+
   const SphinxCat = await hre.ethers.getContractFactory("SphinxCat");
 
   console.log(`Begin to deploy begin=${TIME_START_MYSTREY} uncover=${TIME_UNCOVER_MYSTREY}, root=${root}`)
   const cat = await SphinxCat.deploy(BigNumber.from(TIME_START_MYSTREY), BigNumber.from(TIME_UNCOVER_MYSTREY), root);
 
-  await cat.deployed();
+  // const receipt = await cat.deployed();
+  const deployTx = await cat.deployTransaction
+  const receipt = await deployTx.wait();
+  log_gas_price(receipt);
 
   // // mint 10 个做测试
   // const [owner, account] = ethers.getSigners()
